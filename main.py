3ba9d9a09a2f2fd886d31d2ec542ca92d166a4df
JTRoar <JohnTigerRoar@gmail.com>
2026-01-06 21:15:20 -0500

    C: updated main.py and stats.py, current work in progress for CH3: Report L1: Print a Report

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from stats import get_book_text
+from stats import count_characters
+from stats import word_count
+from stats import sort_dict
+
 
 def main():
-    tha_book = get_book_text("books/frankenstein.txt")
-    print(f"Found {tha_book} total words")
+    with open("books/frankenstein.txt") as f:
+        text_string = f.read().lower()
+    char_count = count_characters(text_string)
+    full_word_count = word_count(text_string)
+    sorted_char_count = sort_dict(char_count)
+    
+    print(full_word_count)
+    #print(char_count)
+    print(char_count)
+    print(sorted_char_count)
 
 main()
--- stats.py
+++ stats.py
@@ -1,11 +1,42 @@
-def get_book_text(path_to_book):
-    with open(path_to_book) as f:
-        text = f.read()
-        # split_text = text.split() // only used if using the for loop below
-        word_count = len(text.split()) # the easy way 
+ 
+def word_count(text_string):
+    #split_text = text_string.split() // only used if using the for loop below
+    word_count = len(text_string.split()) # the easy way 
+     
     """ the hard way
         word_count = 0 
         for word in split_text:
-        word_count += 1 """
+        word_count += 1 """ 
     
-    return word_count
+    return word_count 
+
+def count_characters(text_string):
+    count_dict = {}
+        
+    """For loop that goes through each character, adds to count_dict, and sets the value to 1
+    if it's not already in the count_dict. If it IS in count_dict, it will increment
+    with the else statement"""
+    
+    for current_character in text_string:
+        if current_character not in count_dict:
+            count_dict[current_character] = 1
+        else:
+            count_dict[current_character] += 1   
+        
+    return count_dict
+
+def sort_dict(dict_to_sort):
+    sorted_LIST_of_dicts = []
+
+    for dict in dict_to_sort:
+        sorted_LIST_of_dicts.append(dict)
+    
+    print(sorted_LIST_of_dicts)
+
+
+    return sorted_LIST_of_dicts
+
+""" references:
+    #print(count_dict)     # current_character is the individual character
+    #print(count_dict[current_character])  #count_dict[current_character] returns the value of the key
+"""
