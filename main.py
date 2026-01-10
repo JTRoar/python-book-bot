ecc2458563ee27763a1e63d75bb3e727999f842b
JTRoar <JohnTigerRoar@gmail.com>
2026-01-09 21:40:49 -0500

    E: Finished first go

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 from stats import count_characters
 from stats import word_count
 from stats import make_list_of_dicts
-from stats import sort_on
+import sys
+
 
 
 def main():
-    with open("books/frankenstein.txt") as f:
-        text_string = f.read().lower()
-    char_count = count_characters(text_string)
-    full_word_count = word_count(text_string)
-    sorted_char_count = make_list_of_dicts(char_count)
+    print(sys.argv)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        with open(sys.argv[1]) as f:
+            text_string = f.read().lower()
+        char_count = count_characters(text_string)
+        full_word_count = word_count(text_string)
+        sorted_char_count = make_list_of_dicts(char_count)
     
     
     
     
     
-    print(f"============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------")
-    print(f"Found {full_word_count} total words")
-    print("--------- Character Count -------")
+        print(f"============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------")
+        print(f"Found {full_word_count} total words")
+        print("--------- Character Count -------")
     #print(char_count)
     #print(char_count)
     #sorted_char_count.sort(reverse=True, key=sort_on)
-    for item in sorted_char_count:
-        ch = item["char"]
-        count = item["num"]
-        if ch.isalpha():
-            print(f"{ch}: {count}")
-    print("============= END ===============")
+        for item in sorted_char_count:
+            ch = item["char"]
+            count = item["num"]
+            if ch.isalpha():
+                print(f"{ch}: {count}")
+        print("============= END ===============")
 
 main()
